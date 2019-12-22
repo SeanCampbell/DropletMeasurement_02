329de1bd1b0edb2201514ab0a8e8ea0c463db371
Sean Campbell <seancampbell04@gmail.com>
2019-12-22 10:40:40 -0500

    MVP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSelectModule, MatListModule, MatTableModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ChooseFileComponent } from './choose-file.component';
+import { ChooseFileService } from './choose-file.service';
+import { DropletCanvasComponent } from './droplet-canvas.component';
+import { MeasureComponent } from './measure.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChooseFileComponent,
+    DropletCanvasComponent,
+    MeasureComponent,
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatListModule,
+    MatTableModule,
   ],
-  providers: [],
+  providers: [
+    ChooseFileService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
